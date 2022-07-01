e087d301888e8134b7d2ba08d0a276dcb71b6a27
thecoderzone <sylversipi@yahoo.co.uk>
2022-07-01 16:47:40 -0700

    4 print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,16 @@
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
-	ch++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
 
