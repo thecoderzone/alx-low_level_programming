062f268cb4dc28da4e875dfd796442d6b11bd190
thecoderzone <sylversipi@yahoo.co.uk>
2022-07-01 18:12:15 -0700

    print 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,15 @@
 int main(void)
 {
 	int n;
-	int n;
+	char ch;
 
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
