45107465f7d86ba2f2630102a3732ad71e9c4011
thecoderzone <sylversipi@yahoo.co.uk>
2022-07-05 10:50:20 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void)
+{
+ 	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return 0;
+}
