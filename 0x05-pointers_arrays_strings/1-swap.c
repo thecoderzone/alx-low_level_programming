307b6b29c90a923533443553b8a75da9f08b30bf
thecoderzone <sylversipi@yahoo.co.uk>
2022-07-11 10:53:33 -0700

    Don't swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ *
+ * @a: An integer to swap
+ * Another integer to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
