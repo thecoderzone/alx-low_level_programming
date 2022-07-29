73d8ed92520065978369723791d432576160c731
thecoderzone <sylversipi@yahoo.co.uk>
2022-07-28 18:30:39 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdlib.h>
 
 /**
  * _realloc - Reallocates a memory block using malloc and free.
@@ -13,41 +14,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	char *nptr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
 
 	if (ptr == NULL)
 	{
-		mem = malloc(new_size);
+		nptr = malloc(new_size);
 
-		if (mem == NULL);
-		return (NULL);
+		if (nptr == NULL)
+			return (NULL);
+
+		return (nptr);
 	}
-
-	if (new_size == 0 && ptr != NULL)
+	else
 	{
-		free(ptr);
-		return (NULL);
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
 	}
 
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
+	nptr = malloc(new_size);
 
-	if (mem == NULL)
+	if (nptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		free(ptr);
-		return (NULL);
+		nptr[i] = ((char *) ptr)[i];
 	}
-
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
 
 	free(ptr);
-	return (mem);
+	return (nptr);
 }
