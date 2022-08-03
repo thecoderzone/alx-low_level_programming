868a92c146ce0cf63034e01ba76c7b12cb8b54a5
thecoderzone <sylversipi@yahoo.co.uk>
2022-08-03 14:18:26 -0700

    whtas my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a name
-* print_name - prints a name
-* @f: function to print with
-*
-* Return: void
-*/
+ * print_name - prints a name
+ * print_name - prints a name
+ * @f: function to print with
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
